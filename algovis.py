f1363170ceb2b993b14281f74b67eea1c362942a
magnusrodseth <59113973+magnusrodseth@users.noreply.github.com>
2020-07-09 18:18:18 +0200

    Changed 'home' to 'index'

--- algovis.py
+++ algovis.py
@@ -4,9 +4,9 @@ app = Flask(__name__)
 
 
 @app.route('/')
-@app.route('/home')
-def home():
-    return render_template('home.html',
+@app.route('/index')
+def index():
+    return render_template('index.html',
                            title='Home',
                            is_algorithm=False)
 
